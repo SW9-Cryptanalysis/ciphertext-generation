74ed63bf20ea9e56396bdc208d7649df47b42dcb
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-09-23 13:17:31 +0200

    Move fetcher to src, add dependencies

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from ciphertext-generation!")
+from fetcher import Fetcher
 
+def main():
+	print("Hello from ciphertext-generation!")
+
+	fetcher = Fetcher()
+	book_text = fetcher.fetch_random_book_text()
+	if book_text:
+		print("Fetched book text successfully!")
+	else:
+		print("Failed to fetch book text.")
 
 if __name__ == "__main__":
-    main()
+	main()
