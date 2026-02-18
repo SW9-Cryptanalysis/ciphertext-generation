c637592263adad6a1b865c9f38ed193968a24515
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2026-02-18 22:20:11 +0100

    feat: script for generating ciphers from text stream

--- src/gen_training.py
+++ src/gen_training.py
@@ -1,17 +1,22 @@
 import os
 from utils.logging import get_colored_logger
 from drive.cipher_manager import CipherManager
+from fetching.text_splits import get_text_stream
 
 if __name__ == "__main__":
 	log = get_colored_logger("gen_training")
-	folder_id = os.getenv("FOLDER_ID")
+	folder_id = os.getenv("FOLDER_ID_TRAIN")
 
 	if not folder_id:
 		raise OSError(
 			"FOLDER_ID environment variable not set. Please set it before running.",
 		)
 
-	manager = CipherManager(folder_id=folder_id)
+	text_stream = get_text_stream()
+
+	manager = CipherManager(
+		folder_id=folder_id, text_stream_source=text_stream, total_count=1_000_000,
+	)
 
 	try:
 		manager.execute()
