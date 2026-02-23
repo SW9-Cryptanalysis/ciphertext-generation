cb5fbda4267d6c1e9876b113e0e91ec00e99634e
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2026-02-23 11:04:13 +0100

    !feat: added capability to upload metadata

--- src/drive/cipher_manager.py
+++ src/drive/cipher_manager.py
@@ -2,6 +2,7 @@ import multiprocessing as mp
 from utils.logging import get_colored_logger
 import os
 from typing import Iterable, Any
+import json
 
 from drive.drive_uploader import DriveUploader, DriveUploaderConfig
 from drive.cipher_producer import CipherProducer
@@ -89,12 +90,7 @@ class CipherManager:
 
 		count_fed = 0
 		try:
-			for split, text_data in self.stream:
-				self.job_queue.put((split, text_data))
-				count_fed += 1
-
-				if count_fed % 1000 == 0:
-					log.info(f"Fed {count_fed} texts to workers...")
+			self._feeder_stream()
 
 		except KeyboardInterrupt:
 			log.warning("Job interrupted! Stopping...")
@@ -108,14 +104,49 @@ class CipherManager:
 		for p in workers:
 			p.join()
 
+		self._upload_metadata()
 		self.result_queue.put(self.SENTINEL)
 
 		uploader.join()
 
-		peak_value = self.max_symbol_id.value
-
 		log.info("=" * 40)
 		log.info("JOB COMPLETE")
 		log.info(f"Total ciphers fed: {count_fed}")
-		log.info(f"Peak homophone ID (Vocab Size): {peak_value}")
+		log.info(f"Peak homophone ID (Vocab Size): {self.max_symbol_id.value}")
 		log.info("=" * 40)
+
+	def _upload_metadata(self) -> None:
+		"""Upload the metadata file to Google Drive.
+
+		This method uploads the metadata file to Google Drive, which contains
+		the peak homophone ID (Vocab Size) of the generated ciphers.
+
+		"""
+		log.info("Uploading metadata file...")
+		metadata_filename = "metadata.json"
+		metadata_bytes = json.dumps({"max_symbol_id": self.max_symbol_id.value}).encode(
+			"utf-8",
+		)
+		self.result_queue.put(("metadata", metadata_filename, metadata_bytes))
+
+	def _feeder_stream(self) -> None:
+		"""Feed the ciphers to the workers using the job queue.
+
+		This method feeds the ciphers to the workers using the job queue. It
+		handles routing by split and error handling.
+
+		Raises:
+			Exception: If any error occurs during the execution.
+
+		"""
+		log.info("Feeding ciphers to workers...")
+
+		for count_fed, (split, text_data) in enumerate(self.stream):
+			self.job_queue.put((split, text_data))
+
+			if count_fed % 1000 == 0:
+				log.info(f"Fed {count_fed} texts to workers...")
+
+			if count_fed >= self.total_count:
+				log.info(f"Target of {self.total_count} reached. Stopping feeder.")
+				break
