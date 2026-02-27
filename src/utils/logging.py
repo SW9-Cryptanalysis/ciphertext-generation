c3ce5d6b213056b847d4cb355edefb8c8af7e737
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2026-02-27 13:32:43 +0100

    fix: added handler guard  to ensure handler is not duplicated

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -5,8 +5,9 @@ def get_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
 	"""Create logger using the EasyFormatter and return it."""
 	logger = logging.getLogger(name)
 	logger.setLevel(level)
-	handler = logging.StreamHandler()
-	handler.setFormatter(EasyFormatter())
-	logger.addHandler(handler)
+	if not logger.handlers:
+		handler = logging.StreamHandler()
+		handler.setFormatter(EasyFormatter())
+		logger.addHandler(handler)
 
 	return logger
