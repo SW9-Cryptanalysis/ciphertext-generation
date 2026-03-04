6315ebc4265456dfe97b53c24fd9f27006b224f2
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2026-03-04 13:54:19 +0100

    fix: corrected error checking in test_main

--- test/test_main.py
+++ test/test_main.py
@@ -54,7 +54,7 @@ def test_generate_cipher_fails_cipher(mocker, book_text):
 
 	with pytest.raises(ValueError) as excinfo:
 		generate_cipher(1000, 5000, "test_cipher.json", 10)
-	assert "must include a non-empty, lowercase alphabetic string with no spaces in the text field." in str(excinfo.value)
+	assert "Plaintext must be a lowercase alphabetic string with no spaces" in str(excinfo.value)
 
 	# Clean up
 	import os
@@ -89,7 +89,7 @@ def test_generate_monoalphabetic_cipher_fails(mocker, book_text):
 
 	with pytest.raises(ValueError) as excinfo:
 		generate_monoalphabetic_cipher(1000, 5000, "test_mono_cipher.json")
-	assert " must include a non-empty, lowercase alphabetic string with no spaces in the text field." in str(excinfo.value)
+	assert "Plaintext must be a lowercase alphabetic string with no spaces" in str(excinfo.value)
 
 	# Clean up
 	import os
