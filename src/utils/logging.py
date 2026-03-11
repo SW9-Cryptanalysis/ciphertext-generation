9ef51f55e2826dfd73ad5d63b4482e785c6187bd
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2026-03-11 10:03:25 +0100

    feat: added logger getter for tqdm logger (works with progress bars)

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,5 +1,20 @@
 import logging
 from easy_logging import EasyFormatter
+import tqdm
+
+
+class TqdmLoggingHandler(logging.Handler):
+	"""Custom logging handler routing logs through tqdm."""
+
+	def emit(self, record: logging.LogRecord) -> None:
+		"""Format the log record and output it safely above the progress bars."""
+		try:
+			msg = self.format(record)
+			tqdm.tqdm.write(msg)
+			self.flush()
+		except Exception:
+			self.handleError(record)
+
 
 def get_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
 	"""Create logger using the EasyFormatter and return it."""
@@ -9,5 +24,30 @@ def get_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
 		handler = logging.StreamHandler()
 		handler.setFormatter(EasyFormatter())
 		logger.addHandler(handler)
+
+	return logger
+
+
+def get_logger_tqdm(name: str, level: int = logging.DEBUG) -> logging.Logger:
+	"""Initialize and return a logger with a custom formatter and tqdm handler.
+
+	Args:
+		name (str): The name of the logger.
+
+	Returns:
+		logging.Logger: The fully configured logger.
+
+	"""
+	logger = logging.getLogger(name)
+	logger.setLevel(level)
+
+	if not logger.handlers:
+		tqdm_handler = TqdmLoggingHandler()
+
+		tqdm_handler.setFormatter(EasyFormatter())
+
+		logger.addHandler(tqdm_handler)
+
+		logger.propagate = False
 
 	return logger
